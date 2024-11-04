37c7d3904d30ca05f6c653a71020d95d53ffb1bc
Surya-Premkumar <Surya.premkumar@city.ac.uk>
2024-11-04 16:46:18 +0000

    Added compiler
    Added compiler

---
+++ compiler.py
@@ -0,0 +1,49 @@
+code=str(input())
+code=list(code)
+array= [0] * 16
+pointer=0
+
+skiploop=0
+counter=0
+while counter<len(code):
+    if code[counter]=="+":
+        array[pointer]+=1
+    elif code[counter]=="-":
+        array[pointer]-=1
+    elif code[counter]==">":
+        pointer+=1
+    elif code[counter]=="<":
+        pointer-=1
+    elif code[counter]==".":
+        print(array[pointer])
+    elif code[counter]==",":
+        array[pointer]=int(input())
+    elif code[counter]=="[":
+        if array[pointer]==0:
+            for counter2 in range(counter+1,len(code)-1):
+                if code[counter2]=="[":
+                    skiploop+=1
+                elif code[counter2]=="]":
+                    skiploop-=1
+                    if skiploop==0:
+                        counter=counter2
+                    break
+    elif code[counter]=="]":
+        if array[pointer]!=0:
+            for counter2 in range(counter-1,0,-1):
+                if code[counter2]=="]":
+                    skiploop+=1
+                elif code[counter2]=="[":
+                    if skiploop!=0:
+                        skiploop-=1
+                    if skiploop==0:
+                        counter=counter2
+                    break
+    else:
+        print("Error: Unknown character , Line", counter+1)
+    print(" ",counter,array[1])
+    counter+=1
+   
+   
+   
+#inputcommand(code[counter],code)
